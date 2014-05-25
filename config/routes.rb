910766b699d7ac237116501f735815d2bae7c411
ganesh <ganeshganga.deva@gmail.com>
2014-05-25 20:30:35 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+ActionController::Routing::Routes.draw do |map|
+  map.signup 'signup', :controller => 'users', :action => 'new'
+  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
+  map.login 'login', :controller => 'sessions', :action => 'new'
+  map.resources :sessions
+  map.resources :users
+  map.resources :friendships
+  map.resources :posts
+  map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/:id.:format'
+  map.root :controller => "posts"
+end
